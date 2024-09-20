0f038a72cb7cc1256a27a24182620ade0e7d931b
Diana <diana101210@gmail.com>
2024-09-20 18:51:27 -0500

    create validation of fields, custom error and funcionality about authorize roles

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,1 +1,36 @@
-// error generaly
+import { NextFunction, Request, Response } from "express";
+
+//Extends Class of Error permit to create a custom error
+export class ApiError extends Error{
+
+
+  status: number; // status Error
+  details?: Record<string, any>; // Details Error <nameError, error>
+  
+  constructor (message: string, status: number, details?:Record<string, any>){
+     super(message);
+     this.status = status;
+     this.details = details;
+  }
+
+}
+
+
+ export default function errorHandler(error:Error, _req: Request, res: Response, _next: NextFunction) {
+    if(error instanceof ApiError){
+        res.status(error.status).json({
+            ok: false,
+            error :{
+                message: error.message,
+                details: error.details
+            },
+        });
+    }else{
+        res.status(500).json({
+            ok:false,
+            error:{
+                message: "Error internal on Server"
+            }
+        });
+    }
+  }
